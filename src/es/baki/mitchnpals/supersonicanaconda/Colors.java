42a966d6f9f4761a201051bcb13e6164d50c08dd
Jon Bakies <jon@baki.es>
2016-12-05 16:50:45 -0500

    Worked on colors Added div to stack class

--- src/es/baki/mitchnpals/supersonicanaconda/Colors.java
+++ src/es/baki/mitchnpals/supersonicanaconda/Colors.java
@@ -2,30 +2,35 @@ package es.baki.mitchnpals.supersonicanaconda;
 
 public class Colors {
 	public enum Color {
-		LIGHT_RED (0, 0),
-		RED (0, 1),
-		DARK_RED (0, 2),
-		LIGHT_YELLOW (1, 0),
-		YELLOW (1, 1),
-		DARK_YELLOW (1, 2),
-		LIGHT_GREEN (2, 0),
-		GREEN (2, 1),
-		DARK_GREEN (2, 2),
-		LIGHT_CYAN (3, 0),
-		CYAN (3, 1),
-		DARK_CYAN (3, 2),
-		LIGHT_BLUE (4, 0),
-		BLUE (4, 1),
-		DARK_BLUE (4, 2),
-		LIGHT_MAGENTA (5, 0),
-		MAGENTA (5, 1),
-		DARK_MAGENTA (5, 1);
+		WHITE(0,0,0),
+		BLACK(-1,0,0),
+		
+		LIGHT_RED (1,0, 0),
+		RED (2,0, 1),
+		DARK_RED (3,0, 2),
+		LIGHT_YELLOW (4,1, 0),
+		YELLOW (5,1, 1),
+		DARK_YELLOW (6,1, 2),
+		LIGHT_GREEN (7,2, 0),
+		GREEN (8,2, 1),
+		DARK_GREEN (9,2, 2),
+		LIGHT_CYAN (10,3, 0),
+		CYAN (11,3, 1),
+		DARK_CYAN (12,3, 2),
+		LIGHT_BLUE (13,4, 0),
+		BLUE (14,4, 1),
+		DARK_BLUE (15,4, 2),
+		LIGHT_MAGENTA (16,5, 0),
+		MAGENTA (17,5, 1),
+		DARK_MAGENTA (18,5, 1);
 		
 		
-		private int hue, darkness;
-		Color(int hue, int darkness) {
+		private final int index, hue, darkness;
+		Color(int index, int hue, int darkness) {
 				this.hue = hue;
 				this.darkness = darkness;
+				
+				this.index = index;
 		}
 		
 		public int getHue() {
@@ -36,6 +41,9 @@ public class Colors {
 		}
 		
 		public int getHueDifference(Color c2) {
+			if (index == -1 || index == 0 || c2.index == 0 || c2.index == -1) { 
+				return -1;
+			}
 			int ret = c2.getHue() - hue;
 			if (ret < 0) {
 				ret += 6;
@@ -44,6 +52,9 @@ public class Colors {
 			return ret;
 		}		
 		public int getDarknessDifference(Color c2) {
+			if (index == -1 || index == 0 || c2.index == 0 || c2.index == -1) { 
+				return -1;
+			}
 			int ret = c2.getDarkness() - darkness;
 			if (ret < 0) {
 				ret += 4;
@@ -53,9 +64,21 @@ public class Colors {
 		}
 	}
 	
+	/**
+	 * @deprecated
+	 * @param c1
+	 * @param c2
+	 * @return
+	 */
 	public static int getHueDifference(Color c1, Color c2) {
 		return c1.getHueDifference(c2);
 	}
+	/**
+	 * @deprecated
+	 * @param c1
+	 * @param c2
+	 * @return
+	 */
 	public static int getDarknessDifference(Color c1, Color c2) {
 		return c1.getDarknessDifference(c2);
 	}
