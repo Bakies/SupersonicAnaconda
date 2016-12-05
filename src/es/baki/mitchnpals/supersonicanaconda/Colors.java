6d88949733178f65cc36576800782db6e2892ceb
Jon Bakies <jon@baki.es>
2016-12-05 16:26:23 -0500

    Fix modes, made colors work maybe

--- src/es/baki/mitchnpals/supersonicanaconda/Colors.java
+++ src/es/baki/mitchnpals/supersonicanaconda/Colors.java
@@ -2,31 +2,62 @@ package es.baki.mitchnpals.supersonicanaconda;
 
 public class Colors {
 	public enum Color {
-		LIGHT_RED,
-		RED,
-		DARK_RED,
-		LIGHT_YELLOW,
-		YELLOW,
-		DARK_YELLOW,
-		LIGHT_GREEN,
-		GREEN,
-		DARK_GREEN,
-		LIGHT_CYAN,
-		CYAN,
-		DARK_CYAN,
-		LIGHT_BLUE,
-		BLUE,
-		DARK_BLUE,
-		LIGHT_MAGENTA,
-		MAGENTA,
-		DARK_MAGENTA
+		LIGHT_RED (0, 0),
+		RED (0, 1),
+		DARK_RED (0, 2),
+		LIGHT_YELLOW (1, 0),
+		YELLOW (1, 1),
+		DARK_YELLOW (1, 2),
+		LIGHT_GREEN (2, 0),
+		GREEN (2, 1),
+		DARK_GREEN (2, 2),
+		LIGHT_CYAN (3, 0),
+		CYAN (3, 1),
+		DARK_CYAN (3, 2),
+		LIGHT_BLUE (4, 0),
+		BLUE (4, 1),
+		DARK_BLUE (4, 2),
+		LIGHT_MAGENTA (5, 0),
+		MAGENTA (5, 1),
+		DARK_MAGENTA (5, 1);
+		
+		
+		private int hue, darkness;
+		Color(int hue, int darkness) {
+				this.hue = hue;
+				this.darkness = darkness;
+		}
+		
+		public int getHue() {
+			return hue;
+		}
+		public int getDarkness() {
+			return darkness;
+		}
+		
+		public int getHueDifference(Color c2) {
+			int ret = c2.getHue() - hue;
+			if (ret < 0) {
+				ret += 6;
+			}
+			ret %= 5;
+			return ret;
+		}		
+		public int getDarknessDifference(Color c2) {
+			int ret = c2.getDarkness() - darkness;
+			if (ret < 0) {
+				ret += 4;
+			}
+			ret %= 3;
+			return ret;
+		}
 	}
 	
-	public static int getHueDifference() {
-		return 0;
+	public static int getHueDifference(Color c1, Color c2) {
+		return c1.getHueDifference(c2);
 	}
-	public static int getDarknessDifference() {
-		return 0;
+	public static int getDarknessDifference(Color c1, Color c2) {
+		return c1.getDarknessDifference(c2);
 	}
 	
 
--- src/es/baki/mitchnpals/supersonicanaconda/Interpreter.java
+++ src/es/baki/mitchnpals/supersonicanaconda/Interpreter.java
@@ -1,24 +1,6 @@
 package es.baki.mitchnpals.supersonicanaconda;
 
 public class Interpreter {
-	public static Colors LIGHT_RED = new Colors(0,0);
-	public static Colors RED= new Colors(0,1);
-	public static Colors DARK_RED = new Colors(0,2);
-	public static Colors LIGHT_YELLOW = new Colors(1,0);
-	public static Colors YELLOW = new Colors(1,1);
-	public static Colors DARK_YELLOW = new Colors(1,2);
-	public static Colors LIGHT_GREEN = new Colors(2,0);
-	public static Colors GREEN= new Colors(2,1);
-	public static Colors DARK_GREEN= new Colors(2,2);
-	public static Colors LIGHT_CYAN= new Colors(3,0);
-	public static Colors CYAN= new Colors(3,1);
-	public static Colors DARK_CYAN= new Colors(3,2);
-	public static Colors LIGHT_BLUE= new Colors(4,0);
-	public static Colors BLUE= new Colors(4,1);
-	public static Colors DARK_BLUE= new Colors(4,2);
-	public static Colors LIGHT_MAGENTA= new Colors(5,0);
-	public static Colors MAGENTA= new Colors(5,1);
-	public static Colors DARK_MAGENTA= new Colors(5,2);
 	
 	private Stack stack;
 	private Canvas canvas;
