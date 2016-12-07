d5af5ae41b59c686913e0f74d5a04a5961ae030b
Jon Bakies <jon@baki.es>
2016-12-07 16:37:43 -0500

    Working png export

--- src/es/baki/mitchnpals/supersonicanaconda/Canvas.java
+++ src/es/baki/mitchnpals/supersonicanaconda/Canvas.java
@@ -9,7 +9,7 @@ import javax.imageio.ImageIO;
 public class Canvas {
 	private int height, width; 
 	private Color[][] canvas;
-	
+
 	public Canvas(int width, int height, Canvas canvas) {
 		this(height, width);
 		for (int x = 0; x < canvas.width && x < this.width; x++){
@@ -18,11 +18,11 @@ public class Canvas {
 			}
 		}
 	}
-	
+
 	public Canvas(int length) {
 		this(length, length);
 	}
-	
+
 	public Canvas(int width, int height) {
 		this.height = height;
 		this.width = width;
@@ -33,13 +33,13 @@ public class Canvas {
 			}
 		}
 	}
-	
+
 	public void set(int x, int y, Color color) {
 		if (color == null)
 			color = Color.WHITE;
 		canvas[x][y] = color;
 	}
-	
+
 	public int getHeight() {
 		return height;
 	}
@@ -52,11 +52,11 @@ public class Canvas {
 			return null;
 		return canvas[x][y];
 	}
-	
+
 	public void readFromPietFile() {
 		// TODO Read from piet file 
 	}
-	
+
 	public static Canvas readFromPNGFile(String filename) {
 		File file = new File(filename);
 		BufferedImage image ;
@@ -74,69 +74,43 @@ public class Canvas {
 		w = image.getData().getWidth(); 
 		h = image.getData().getHeight();
 		Canvas c = new Canvas(w, h);
-		
+
 		for (int y = 0; y < h; y ++) 
-			for (int x = 0; x < w; x ++){
-				int rgb = image.getRGB(x, y);
-				rgb = rgb & 0xFFFFFF;
-				if (rgb == 0xFFC0C0) 
-					c.set(x, y, Color.LIGHT_RED);
-				else if (rgb == 0xFF0000)
-					c.set(x, y, Color.RED);
-				else if (rgb == 0xC00000) 
-					c.set(x, y, Color.DARK_RED);
-				else if (rgb == 0xFFFFC0)
-					c.set(x, y, Color.LIGHT_YELLOW);
-				else if (rgb == 0xFFFF00)
-					c.set(x, y, Color.YELLOW);
-				else if (rgb == 0xC0C000)
-					c.set(x, y, Color.DARK_YELLOW);
-				else if (rgb == 0xC0FFC0)
-					c.set(x, y, Color.LIGHT_GREEN);
-				else if (rgb == 0x00FF00)
-					c.set(x, y, Color.GREEN);
-				else if (rgb == 0x00C000)
-					c.set(x, y, Color.DARK_GREEN);
-				else if (rgb == 0xC0FfFF)
-					c.set(x, y, Color.LIGHT_CYAN);
-				else if (rgb == 0x00FFFF) 
-					c.set(x, y, Color.CYAN);
-				else if (rgb == 0x00C0C0)
-					c.set(x, y, Color.DARK_CYAN);
-				else if (rgb == 0xC0C0FF)
-					c.set(x, y, Color.LIGHT_BLUE);
-				else if (rgb == 0x0000FF)
-					c.set(x, y, Color.BLUE);
-				else if (rgb == 0x0000C0)
-					c.set(x, y, Color.DARK_BLUE);
-				else if (rgb == 0xFFC0FF)
-					c.set(x, y, Color.LIGHT_MAGENTA);
-				else if (rgb == 0xFF00FF)
-					c.set(x, y, Color.MAGENTA);
-				else if (rgb == 0xC000C0)
-					c.set(x, y, Color.DARK_MAGENTA);
-				else if (rgb == 0xFFFFFF)
-					c.set(x, y, Color.WHITE);
-				else if (rgb == 0x000000)
-					c.set(x, y, Color.BLACK);
-				else {
-					System.err.printf("Unrecognized Color %x%n", rgb);
-					c.set(x, y, Color.WHITE);
-				}
-				
-			}
+			for (int x = 0; x < w; x ++)
+				c.set(x, y, Color.getColorFromRGBInt(image.getRGB(x, y)));
+
 		return c;
 	}
-	
-	public void saveToFile(String filename) {
+
+
+	public void saveToPietFile(String filename) {
 		// TODO Save canvas to file
 	}
-	
+
 	public void exportToPNG(String filename) {
-		// TODO exportToPNG
+		File file = new File(filename);
+		if (!file.exists()) {
+			System.out.println("Making new file");
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.err.println("Failed to make image: " + e.getMessage());
+				return; 
+			}
+		}
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		
+		for (int y = 0; y < height; y ++)
+			for (int x = 0; x < width; x ++)
+				image.setRGB(x, y, Color.getRGB(canvas[x][y]));
+		
+		try {
+			ImageIO.write(image, "png", file);
+		} catch (IOException e) {
+			System.err.println("Failed to write image: " + e.getMessage());
+		}
 	}
-	
+
 	/**
 	 * import from a string where color indexes are separated by spaces
 	 * @param s
@@ -153,7 +127,7 @@ public class Canvas {
 			}
 		}
 	}
-	
+
 	public String exportToIndexString() {
 		String ret = "";
 		for (int y = 0; y < height; y++)
@@ -161,7 +135,7 @@ public class Canvas {
 				ret += canvas[x][y].getIndex() + " ";
 		return ret.trim();
 	}
-	
+
 	public String toString() {
 		StringBuilder ret = new StringBuilder();
 		for (int y = 0; y < width; y ++) {
@@ -172,7 +146,7 @@ public class Canvas {
 		}
 		return ret.toString();
 	}
-	
+
 	public String toReadableString() {
 		StringBuilder ret = new StringBuilder();
 		for (int y = 0; y < width; y ++) {
@@ -184,8 +158,8 @@ public class Canvas {
 		}
 		return ret.substring(0, ret.length() - 1);
 	}
-	
-	
+
+
 	public static void main(String...strings) {
 		Canvas c = new Canvas(10);
 		c.set(0, 0, Color.BLACK);
@@ -193,5 +167,7 @@ public class Canvas {
 		System.out.println(c);
 		System.out.println(c.toReadableString());
 		
+		c.exportToPNG("exporttest.png");
+
 	}
 }
--- src/es/baki/mitchnpals/supersonicanaconda/Color.java
+++ src/es/baki/mitchnpals/supersonicanaconda/Color.java
@@ -80,4 +80,104 @@ public enum Color {
 	public int getIndex() {
 		return index;
 	}
+	
+	/**
+	 * will return 
+	 * @param rgb
+	 * @return
+	 */
+	public static Color getColorFromRGBInt(int rgb) {
+		rgb = 0xFFFFFF & rgb;
+		switch (rgb) {
+		case 0xFFC0C0:
+		return Color.LIGHT_RED;
+		case 0xFF0000:
+		return Color.RED;
+		case 0xC00000:
+		return Color.DARK_RED;
+		case 0xFFFFC0:
+		return Color.LIGHT_YELLOW;
+		case 0xFFFF00:
+		return Color.YELLOW;
+		case 0xC0C000:
+		return Color.DARK_YELLOW;
+		case 0xC0FFC0:
+		return Color.LIGHT_GREEN;
+		case 0x00FF00:
+		return Color.GREEN;
+		case 0x00C000:
+		return Color.DARK_GREEN;
+		case 0xC0FfFF:
+		return Color.LIGHT_CYAN;
+		case 0x00FFFF:
+		return Color.CYAN;
+		case 0x00C0C0:
+		return Color.DARK_CYAN;
+		case 0xC0C0FF:
+		return Color.LIGHT_BLUE;
+		case 0x0000FF:
+		return Color.BLUE;
+		case 0x0000C0:
+		return Color.DARK_BLUE;
+		case 0xFFC0FF:
+		return Color.LIGHT_MAGENTA;
+		case 0xFF00FF:
+		return Color.MAGENTA;
+		case 0xC000C0:
+		return Color.DARK_MAGENTA;
+		case 0xFFFFFF:
+		return Color.WHITE;
+		case 0x000000:
+		return Color.BLACK;
+		default:
+			System.err.println("Unknown Color " + rgb);
+			return Color.WHITE;
+		}
+	}
+	public static int getRGB(Color c) {
+		switch (c) {
+		case LIGHT_RED:
+			return 0xFFC0C0;
+		case RED:
+			return 0xFF0000;
+		case DARK_RED:
+			return 0xC00000;
+		case LIGHT_YELLOW:
+			return 0xFFFFC0;
+		case YELLOW:
+			return 0xFFFF00;
+		case DARK_YELLOW:
+			return 0xC0C000;
+		case LIGHT_GREEN:
+			return 0xC0FFC0;
+		case GREEN:
+			return 0x00FF00;
+		case DARK_GREEN:
+			return 0x00C000;
+		case LIGHT_CYAN:
+			return 0xC0FfFF;
+		case CYAN:
+			return 0x00FFFF;
+		case DARK_CYAN:
+			return 0x00C0C0;
+		case LIGHT_BLUE:
+			return 0xC0C0FF;
+		case BLUE:
+			return 0x0000FF;
+		case DARK_BLUE:
+			return 0x0000C0;
+		case LIGHT_MAGENTA:
+			return 0xFFC0FF;
+		case MAGENTA:
+			return 0xFF00FF;
+		case DARK_MAGENTA:
+			return 0xC000C0;
+		case WHITE:
+			return 0xFFFFFF;
+		case BLACK:
+			return 0x000000;
+		default:
+			return 0xFFFFFF;
+		}
+	}
 }
