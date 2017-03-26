b7385f8c3ef4ec3b650d2db7119b0020118b5efc
Jon Bakies <jon@baki.es>
2017-03-26 14:23:18 -0400

    Added buggy click and drag

--- src/es/baki/mitchnpals/supersonicanaconda/gui/CanvasPanel.java
+++ src/es/baki/mitchnpals/supersonicanaconda/gui/CanvasPanel.java
@@ -11,7 +11,12 @@ import java.awt.Color;
 public class CanvasPanel extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	private JPanel[][] panels;
+	
+	private boolean mouseDown = false;
+	private int mouseDownXCoord = 0, mouseDownYCoord = 0; 
+	private int mouseXCoord = 0, mouseYCoord = 0;
+	
+	private CodelPanel[][] panels;
 	private Frame f;
 	public CanvasPanel(Frame f){
 		super(new GridLayout(10,10));
@@ -30,7 +35,7 @@ public class CanvasPanel extends JPanel {
 	}
 
 	public void placePanels(int width, int height){
-		panels = new JPanel[width][height];		
+		panels = new CodelPanel[width][height];		
 
 		for(int i = 0; i < width; i++) {
 			for(int j = 0; j < height; j++){
@@ -71,32 +76,61 @@ public class CanvasPanel extends JPanel {
 			return this.codelX;
 		}
 
+		public void setBackground(Color bg) { 
+			super.setBackground(bg);
+		}
+		
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			this.setBackground(f.getSelectedColor());
+		}
+		
+		public void setBorderColor(Color borderColor) {
+				super.setBorder(BorderFactory.createLineBorder(borderColor));
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			this.setBorder(BorderFactory.createLineBorder(Color.yellow));
 			
+			mouseXCoord = codelX;
+			mouseYCoord = codelY;
+			
+			for (CodelPanel[] x : panels)
+				for (CodelPanel y : x)
+					y.setBorderColor(Color.BLACK);
+			if (mouseDown) {
+				for (int x = Math.min(mouseDownXCoord, codelX); x <= Math.max(mouseDownXCoord, codelX); x++) {
+					for (int y = Math.min(mouseDownYCoord, codelY); y <= Math.max(mouseDownYCoord, codelY); y ++) {
+						panels[x][y].setBorderColor(Color.blue);
+					}
+				}
+			}
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
 			this.setBorder(BorderFactory.createLineBorder(Color.black));
-			
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
+			mouseDownXCoord = codelX;
+			mouseDownYCoord = codelY;
+			mouseDown = true;
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
+			for (CodelPanel[] x : panels)
+				for (CodelPanel y : x)
+					y.setBorderColor(Color.BLACK);
+			for (int x = Math.min(mouseDownXCoord, mouseXCoord); x <= Math.max(mouseDownXCoord, mouseXCoord); x++) {
+				for (int y = Math.min(mouseDownYCoord, mouseXCoord); y <= Math.max(mouseDownYCoord, mouseYCoord); y ++) {
+					panels[x][y].setBackground(f.getSelectedColor());
+				}
+			}
+			mouseDown = false;
 			
 		}
 		
