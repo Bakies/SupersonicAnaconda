5fdf7eb5340ff58f8b83c604a658f1ec91d5dcde
Jon Bakies <jon@baki.es>
2017-06-14 13:36:01 -0400

    Formatting and did some todos to show very generic message dialogs

--- src/es/baki/mitchnpals/supersonicanaconda/gui/Frame.java
+++ src/es/baki/mitchnpals/supersonicanaconda/gui/Frame.java
@@ -29,7 +29,7 @@ import es.baki.mitchnpals.supersonicanaconda.Canvas;
 public class Frame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private GridBagLayout layoutManager;
-	
+
 	// Menu bar stuff
 	private MenuBar menubar;
 	private Menu fileMenu;
@@ -37,71 +37,66 @@ public class Frame extends JFrame {
 	private FileOpenButton open;
 	private FileNewButton newCanvas;
 	private FileSaveButton save;
-	private EditUndoButton undo; 
-	
+	private EditUndoButton undo;
+
 	// Panels
 	private JPanel ioPanel;
 	private JLabel ioTitle;
 	private JTextField ioInput;
 	private JTextArea ioOutput;
-	
+
 	private JPanel debugPanel;
-	private JLabel debugTitle; 
-	private JButton debugRunButton; 
+	private JLabel debugTitle;
+	private JButton debugRunButton;
 	private JButton debugStepButton;
 	private JButton debugStopButton;
-	
+
 	private JPanel colorsPickerPanel;
 	private JLabel colorPickerTitle;
 	private JPanel selectedColorPanel;
-	private JPanel  lred, red, dred,
-					lyel, yel, dyel,
-					lgrn, grn, dgrn,
-					lcyn, cyn, dcyn,
-					lblu, blu, dblu,
-					lmag, mag, dmag,
-					black, white;
-	
-	private JPanel toolPickerPanel; 
+	private JPanel lred, red, dred, lyel, yel, dyel, lgrn, grn, dgrn, lcyn, cyn, dcyn, lblu, blu, dblu, lmag, mag, dmag,
+			black, white;
+
+	private JPanel toolPickerPanel;
 	private JLabel toolPickerTitle;
-	
+
 	private CanvasPanel canvasPanel;
 
 	private IDE ide;
-	
-	public Color getSelectedColor() { 
+
+	public Color getSelectedColor() {
 		return selectedColorPanel.getBackground();
 	}
-	
+
 	public Frame(IDE ide) {
 		super("Supersonic Anaconda");
 		this.ide = ide;
-		
+
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		layoutManager = new GridBagLayout();
 		this.setLayout(layoutManager);
 		GridBagConstraints c = new GridBagConstraints();
-		
+
 		makeIOPanel();
-		c.fill  = GridBagConstraints.BOTH;
+		c.fill = GridBagConstraints.BOTH;
 		c.gridx = 0;
-		c.gridy = 0; 
+		c.gridy = 0;
 		this.add(ioPanel, c);
-		
+
 		makeDebugPanel();
 		c.gridy = 1;
 		this.add(debugPanel, c);
-		
+
 		makeColorPickerPanel();
 		c.gridx = 1;
 		c.gridy = 0;
 		this.add(colorsPickerPanel, c);
-		
+
 		makeToolPickerPicker();
-		c.gridx = 1; 
+		c.gridx = 1;
 		c.gridy = 1;
 		this.add(toolPickerPanel, c);
-		
+
 		canvasPanel = new CanvasPanel(10, 10, this);
 		canvasPanel.setAutoscrolls(true);
 		c.fill = GridBagConstraints.NONE;
@@ -110,26 +105,26 @@ public class Frame extends JFrame {
 		c.gridheight = 2;
 		c.gridwidth = 5;
 		this.add(canvasPanel, c);
-		
+
 		makeMenuBar();
 		this.setMenuBar(menubar);
-		
+
 		this.pack();
 		this.setVisible(true);
 		this.setMinimumSize(new Dimension(this.getWidth(), this.getHeight()));
-		
+
 	}
-	
+
 	private void makeNewCanvas(Canvas canvas) {
 		canvasPanel.setCodelGridSize(canvas.getWidth(), canvas.getHeight());
 		this.pack();
-		for (int x = 0; x < canvas.getWidth(); x ++) {
+		for (int x = 0; x < canvas.getWidth(); x++) {
 			for (int y = 0; y < canvas.getHeight(); y++) {
 				canvasPanel.setPanelColor(x, y, canvas.getAwtColor(x, y));
 			}
 		}
 	}
-	
+
 	private void makeToolPickerPicker() {
 		toolPickerPanel = new JPanel();
 		toolPickerPanel.setLayout(new BoxLayout(toolPickerPanel, BoxLayout.Y_AXIS));
@@ -148,7 +143,6 @@ public class Frame extends JFrame {
 		debugRunButton = new JButton("Run");
 		debugPanel.add(debugRunButton);
 		debugRunButton.addActionListener(new ButtonRunActionListener(this, ide));
-		
 
 		debugStopButton = new JButton("Stop");
 		debugPanel.add(debugStopButton);
@@ -171,190 +165,196 @@ public class Frame extends JFrame {
 		selectedColorPanel.setBackground(Color.red);
 		colorsPickerPanel.add(selectedColorPanel);
 		colorsPickerPanel.add(new JPanel());
-		
+
 		JPanel colors = new JPanel();
 		colors.setLayout(new GridLayout(7, 3, 3, 3));
-		
+
 		lred = new ColorSelectorPanel(this);
 		lred.setBackground(new Color(0xFFC0C0));
 		colors.add(lred);
-		
+
 		red = new ColorSelectorPanel(this);
 		red.setBackground(new Color(0xFF0000));
 		colors.add(red);
-		
+
 		dred = new ColorSelectorPanel(this);
 		dred.setBackground(new Color(0xC00000));
 		colors.add(dred);
-		
+
 		lyel = new ColorSelectorPanel(this);
 		lyel.setBackground(new Color(0xFFFFC0));
 		colors.add(lyel);
-		
+
 		yel = new ColorSelectorPanel(this);
 		yel.setBackground(new Color(0xFFFF00));
 		colors.add(yel);
-		
+
 		dyel = new ColorSelectorPanel(this);
 		dyel.setBackground(new Color(0xC0C000));
 		colors.add(dyel);
-		
+
 		lgrn = new ColorSelectorPanel(this);
 		lgrn.setBackground(new Color(0xC0FFC0));
 		colors.add(lgrn);
-		
+
 		grn = new ColorSelectorPanel(this);
 		grn.setBackground(new Color(0x00FF00));
 		colors.add(grn);
-		
+
 		dgrn = new ColorSelectorPanel(this);
 		dgrn.setBackground(new Color(0x00C000));
 		colors.add(dgrn);
-		
+
 		lcyn = new ColorSelectorPanel(this);
 		lcyn.setBackground(new Color(0xC0FfFF));
 		colors.add(lcyn);
-		
+
 		cyn = new ColorSelectorPanel(this);
 		cyn.setBackground(new Color(0x00FFFF));
 		colors.add(cyn);
-		
+
 		dcyn = new ColorSelectorPanel(this);
 		dcyn.setBackground(new Color(0x00C0C0));
 		colors.add(dcyn);
-		
+
 		lblu = new ColorSelectorPanel(this);
 		lblu.setBackground(new Color(0xC0C0FF));
 		colors.add(lblu);
-		
+
 		blu = new ColorSelectorPanel(this);
 		blu.setBackground(new Color(0x0000FF));
 		colors.add(blu);
-		
+
 		dblu = new ColorSelectorPanel(this);
 		dblu.setBackground(new Color(0x0000C0));
 		colors.add(dblu);
-		
+
 		lmag = new ColorSelectorPanel(this);
 		lmag.setBackground(new Color(0xFFC0FF));
 		colors.add(lmag);
-		
+
 		mag = new ColorSelectorPanel(this);
 		mag.setBackground(new Color(0xFF00FF));
 		colors.add(mag);
-		
+
 		dmag = new ColorSelectorPanel(this);
 		dmag.setBackground(new Color(0xC000C0));
 		colors.add(dmag);
-		
+
 		white = new ColorSelectorPanel(this);
 		white.setBackground(new Color(0xFFFFFF));
 		colors.add(white);
-		
+
 		colors.add(new JPanel());
-		
+
 		black = new ColorSelectorPanel(this);
 		black.setBackground(new Color(0x000000));
 		colors.add(black);
-		
+
 		colorsPickerPanel.add(colors);
 	}
 
 	private void makeMenuBar() {
 		menubar = new MenuBar();
 		fileMenu = new Menu("File");
-		
+
 		// File menu
 		newCanvas = new FileNewButton("New");
 		open = new FileOpenButton(this, "Open");
 		save = new FileSaveButton(this, "Save");
-		
+
 		fileMenu.add(newCanvas);
 		fileMenu.add(open);
 		fileMenu.add(save);
-		
+
 		menubar.add(fileMenu);
-		
+
 		// Edit Menu
 		editMenu = new Menu("Edit");
 		undo = new EditUndoButton(this, "Undo");
-		
+
 	}
-	
+
 	private class FileOpenButton extends MenuItem implements ActionListener {
 		private static final long serialVersionUID = 7904843145726959810L;
-		private Frame parent; 
+		private Frame parent;
+
 		public FileOpenButton(Frame parent, String title) {
 			super(title);
-			
+
 			this.addActionListener(this);
-		} 
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			final JFileChooser fc = new JFileChooser(); 
+			final JFileChooser fc = new JFileChooser();
 			fc.showDialog(parent, "Open");
-			
+
 			File file = fc.getSelectedFile();
 			makeNewCanvas(Canvas.readFromFile(file));
-		} 
-		
+		}
+
 	}
 
 	private class EditUndoButton extends MenuItem implements ActionListener {
 		private static final long serialVersionUID = -8620889261216735117L;
-		private Frame parent; 
+		private Frame parent;
+
 		public EditUndoButton(Frame frame, String title) {
 			super();
 			this.parent = parent;
-			// TODO Auto-generated constructor stub
+			this.addActionListener(this);
 		}
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			parent.canvasPanel.undo();
-		} 
-		
+		}
+
 	}
-	
+
 	private class FileSaveButton extends MenuItem implements ActionListener {
 		private static final long serialVersionUID = 7904843145726959811L;
 		private Frame parent;
+
 		public FileSaveButton(Frame parent, String title) {
 			super(title);
 			this.parent = parent;
 			this.addActionListener(this);
-		} 
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			final JFileChooser fc = new JFileChooser();
 			fc.showDialog(parent, "Save");
-			
+
 			File file = fc.getSelectedFile();
 			System.out.println(getCanvas());
 			getCanvas().exportToPNG(file);
-		} 	
-		
+		}
+
 	}
+
 	private class FileNewButton extends MenuItem implements ActionListener {
 		private static final long serialVersionUID = 7904843145726959812L;
+
 		public FileNewButton(String title) {
 			super(title);
 			this.addActionListener(this);
-		} 
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			makeNewCanvas(new Canvas(1, 10));
-		} 
-		
+		}
+
 	}
 
-	
-	
 	private void makeIOPanel() {
 		ioPanel = new JPanel();
 		ioPanel.setLayout(new GridBagLayout());
-		
-		ioTitle = new JLabel("Input/Output");	
+
+		ioTitle = new JLabel("Input/Output");
 		ioPanel.add(ioTitle);
 
 		ioInput = new JTextField();
@@ -366,12 +366,14 @@ public class Frame extends JFrame {
 
 	@SuppressWarnings("serial")
 	public class ColorSelectorPanel extends JPanel implements MouseListener {
-		private Frame f; 
+		private Frame f;
+
 		public ColorSelectorPanel(Frame f) {
 			this.setBorder(BorderFactory.createLineBorder(Color.WHITE));
 			this.addMouseListener(this);
 			this.f = f;
 		}
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			f.selectedColorPanel.setBackground(this.getBackground());
@@ -380,13 +382,13 @@ public class Frame extends JFrame {
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			this.setBorder(BorderFactory.createLineBorder(Color.black));
-			
+
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
 			this.setBorder(BorderFactory.createLineBorder(Color.white));
-			
+
 		}
 
 		@Override
@@ -397,58 +399,64 @@ public class Frame extends JFrame {
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			// ignored
-		} 
-		
-		
+		}
+
 	}
-	
+
 	public class ButtonStopActionListener implements ActionListener {
 		IDE ide;
-		Frame f; 
+		Frame f;
+
 		public ButtonStopActionListener(Frame f, IDE ide) {
 			this.ide = ide;
-			this.f = f; 
+			this.f = f;
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			ide.stopInterpreter();
-		} 
-		
+		}
+
 	}
+
 	public class ButtonStepActionListener implements ActionListener {
 		IDE ide;
 		Frame f;
+
 		public ButtonStepActionListener(Frame f, IDE ide) {
 			this.f = f;
 			this.ide = ide;
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			ide.stepInterpreter();
-		} 
+		}
 	}
+
 	public class ButtonRunActionListener implements ActionListener {
 		IDE ide;
 		Frame f;
+
 		public ButtonRunActionListener(Frame f, IDE ide) {
 			this.ide = ide;
 			this.f = f;
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			ide.runInterpreter();
-		} 
+		}
 	}
-	
+
 	public Canvas getCanvas() {
 		Canvas canvas = new Canvas(canvasPanel.getWidth(), canvasPanel.getHeight());
-		for (int x = 0; x < canvasPanel.getWidth(); x ++) {
-			for (int y = 0; y < canvasPanel.getHeight(); y ++){
-				// backwards because we are converting out of gui, origins top left instead of bottom right
-				canvas.set(x, y, es.baki.mitchnpals.supersonicanaconda.Color.awtToBakiesColor(canvasPanel.getColorAt(y, x)));
+		for (int x = 0; x < canvasPanel.getWidth(); x++) {
+			for (int y = 0; y < canvasPanel.getHeight(); y++) {
+				// backwards because we are converting out of gui, origins top
+				// left instead of bottom right
+				canvas.set(x, y,
+						es.baki.mitchnpals.supersonicanaconda.Color.awtToBakiesColor(canvasPanel.getColorAt(y, x)));
 			}
 		}
 		return canvas;
--- src/es/baki/mitchnpals/supersonicanaconda/gui/IDE.java
+++ src/es/baki/mitchnpals/supersonicanaconda/gui/IDE.java
@@ -1,88 +1,98 @@
 package es.baki.mitchnpals.supersonicanaconda.gui;
 
+import javax.swing.JOptionPane;
+
 import es.baki.mitchnpals.supersonicanaconda.Canvas;
 import es.baki.mitchnpals.supersonicanaconda.Interpreter;
 
 public class IDE {
-	private Frame frame; 
+	private Frame frame;
 	public Interpreter i;
 	public boolean run = true;
 	private boolean running = false;
 	private boolean firstRun = true;
-	
-	public IDE(int height, int width ) {
+
+	public IDE(int height, int width) {
 		frame = new Frame(this);
-		
+
 	}
+
 	public IDE() {
-		this(10,10);
+		this(10, 10);
 	}
-	
+
 	public static void main(String[] args) {
 		new IDE(10, 10);
 	}
+
 	public void stepInterpreter() {
 		try {
-			//i.step();
-			if(firstRun){
+			// i.step();
+			if (firstRun) {
 				Canvas canvas = frame.getCanvas();
 				i = new Interpreter(canvas, System.in);
 				firstRun = false;
 			}
-			//System.out.println("Step");
+			// System.out.println("Step");
 			i.step();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(this.frame, "The interpreter encountered an error");
 		}
 	}
+
 	public void stopInterpreter() {
 		run = false;
 		running = false;
 		firstRun = true;
-		//System.out.println("Stop");
-		
+		// System.out.println("Stop");
+
 	}
+
 	public void runInterpreter() {
 		run = true;
-		if(firstRun){
+		if (firstRun) {
 			Canvas canvas = frame.getCanvas();
 			i = new Interpreter(canvas, System.in);
 			firstRun = false;
 		}
-		if(!running){
+		if (!running) {
 			RunnableIDE r = new RunnableIDE(this, i);
-			r.start();		
+			r.start();
 		}
 	}
+
 	public Frame getFrame() {
 		return this.frame;
 	}
 
 }
 
-class RunnableIDE implements Runnable{
+class RunnableIDE implements Runnable {
 	private Thread t;
 	IDE ide;
 	Interpreter i;
-	RunnableIDE(IDE ide, Interpreter i){
+
+	RunnableIDE(IDE ide, Interpreter i) {
 		this.ide = ide;
 		this.i = i;
 	}
+
 	public void start() {
 		t = new Thread(this);
 		t.start();
 	}
+
 	@Override
 	public void run() {
-		try{
-			while(ide.run && i.step());
-				//System.out.println("run");
-			
-		}catch (Exception e){
-			// TODO idk man its up to you
+		try {
+			while (ide.run && i.step())
+				;
+			// System.out.println("run");
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(ide.getFrame(), "The interpreter ran into an error and had to stop");
 		}
-		
-		
+
 	}
-	
+
 }
